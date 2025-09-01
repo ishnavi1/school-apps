47f2bd1a0fe2c67b4bfefbddffd28a0067f90690
Vaishnavi Chandar <121337041+ishnavi1@users.noreply.github.com>
2025-09-01 20:52:11 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,8 @@ const nextConfig: NextConfig = {
   images: {
     domains: [
       "yeqhhhhm7rpnmvge.public.blob.vercel-storage.com", // <-- add this
-      "blob.vercel-storage.com" // (optional, for other blobs)
+      "blob.vercel-storage.com", // (optional, for other blobs)
+      "xc61y5gc4apmowaw.public.blob.vercel-storage.com"
     ],
   },
 };
